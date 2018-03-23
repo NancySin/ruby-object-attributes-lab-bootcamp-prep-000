0d8cea42212d88693b5ebab8cee83bc7ca0119dc
nancy sin <nsin@sbcglobal.net>
2018-03-23 20:56:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,4 +4,11 @@ class Dog
   def initialize (breed)
     @breed = breed
   end
-  
+
+  class Dog
+    def name =(dog_name)
+      this_dogs_name = dog_name
+     @name = dog_name
+   end
+    
+      
