072fc161306f85bf2ea2bc47b5475904cad62cc1
nancy sin <nsin@sbcglobal.net>
2018-03-23 21:04:59 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,9 +2,17 @@ class Person
   def initialize (name)
     @name = name
   end
+def name
+  @name
+end
+
 
   class Person
-    def job
+    def initalize (job)
       @job = job
+    end
+
+    def job
+      @job
     end
   end
