97cff145df7a7a0624baa39379ef645bbf2e9919
nancy sin <nsin@sbcglobal.net>
2018-03-23 21:11:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,17 @@
 class Dog
-end
-class Dog
-  def breed = (breed)
-    @breed = breed
+ def name= (name)
+    @name = name
   end
-
-def breed
-  @breed
-end
-end
-
-  class Dog
-    def name =(dog_name)
-      @name = dog_name
-   end
 
   def name
     @name
+  end
+
+  def breed= (breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
   end
 end
